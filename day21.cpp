070e9d2cbb970aa5e5c1a943cc2bf75fe1686c2a
Marcus <marcus.kindberg@protonmail.com>
2021-12-22 12:29:38 +0100

    Day 21 done

--- day21.cpp
+++ day21.cpp
@@ -7,8 +7,7 @@
 
 using namespace std;
 
-static void parse_file(fstream& file, int& player1, int& player2)
-{
+static void parse_file(fstream &file, int &player1, int &player2) {
   string line;
   getline(file, line);
   sscanf(line.c_str(), "Player 1 starting position: %d", &player1);
@@ -19,16 +18,16 @@ static void parse_file(fstream& file, int& player1, int& player2)
 int dieRolls = 0;
 int roll_die() {
   int roll = ++dieRolls;
-  while(roll > 100)
+  while (roll > 100)
     roll -= 100;
   return roll;
 }
 
-int step(int& p) {
-  for(unsigned i = 0; i < 3; ++i) {
+int step(int &p) {
+  for (unsigned i = 0; i < 3; ++i) {
     p = p + roll_die();
   }
-  while(p > 10)
+  while (p > 10)
     p -= 10;
   return p;
 }
@@ -37,80 +36,78 @@ int play(int p1, int p2) {
   int score1 = 0;
   int score2 = 0;
   const int win = 1000;
-  while(score1 < win && score2 < win) {
+  while (score1 < win && score2 < win) {
     score1 += step(p1);
-    if(score1 >= win)
+    if (score1 >= win)
       break;
     score2 += step(p2);
   }
-  cout << dieRolls << ' ' << min(score1, score2) << ' ' << max(score1, score2) << endl;
+  cout << dieRolls << ' ' << min(score1, score2) << ' ' << max(score1, score2)
+       << endl;
   return dieRolls * min(score1, score2);
 }
 
-bool day21(long& part1, long& part2)
-{
+int getWeight(int roll) {
+  if (roll == 3 || roll == 9)
+    return 1;
+  if (roll == 4 || roll == 8)
+    return 3;
+  if (roll == 5 || roll == 7)
+    return 6;
+  //if (roll == 6)
+  return 7;
+}
+
+int step2(int tile, int steps) {
+  int t = tile + steps;
+  if(t > 10)
+    t -= 10;
+  return t;
+}
+
+long win1 = 0;
+long win2 = 0;
+void play2(int score1, int score2, int tile1, int tile2, long weight);
+void play1(int score1, int score2, int tile1, int tile2, long weight) {
+
+  for(unsigned i = 3; i < 10; ++i) {
+    int t1 = step2(tile1, i);
+    int s1 = score1 + t1;
+    if(s1 >= 21) {
+      win1 += weight * getWeight(i);
+    } else {
+      play2(s1, score2, t1, tile2, weight * getWeight(i));
+    }
+  }
+
+}
+void play2(int score1, int score2, int tile1, int tile2, long weight) {
+
+  for(unsigned i = 3; i < 10; ++i) {
+    int t2 = step2(tile2, i);
+    int s2 = score2 + t2;
+    if(s2 >= 21) {
+      win2 += weight * getWeight(i);
+    } else {
+      play1(score1, s2, tile1, t2, weight * getWeight(i));
+    }
+  }
+
+}
+
+bool day21(long &part1, long &part2) {
   fstream file;
   file.open("input/21", ios::in);
-  if(file.is_open()) {
+  if (file.is_open()) {
     int p1, p2;
 
     parse_file(file, p1, p2);
     file.close();
 
     part1 = play(p1, p2);
+    play1(0, 0, p1, p2, 1);
+    part2 = max(win1, win2);
 
-    array<array<array<array<pair<long,long>,10>,10>,22>,22> games;
-    for(unsigned i = 0; i < games[0].size() ; ++i) {
-      for(unsigned j = 0; j < games[0][0].size(); ++j) {
-        for(unsigned k = 0; k < games[0][0][0].size(); ++k) {
-          games[21][i][j][k].first = 1;
-          if(i != 21)
-            games[i][21][j][k].second = 1;
-        }
-      }
-    }
-
-    for(int i = games.size()-2; i >= 0 ; --i) {
-      for(int j = games[0].size()-2; j >= 0 ; --j) {
-        for(int k = 0; k < (int)games[0][0].size() ; ++k) {
-          for(int l = 0; l < (int)games[0][0][0].size() ; ++l) {
-            for(int r = 3; r < 10; ++r) {
-              int weight;
-              if(r == 3 || r == 9)
-                weight = 1;
-              if(r == 4 || r == 8)
-                weight = 3;
-              if(r == 5 || r == 7)
-                weight = 6;
-              if(r == 6)
-                weight = 7;
-              int score1 = i + 1;
-              int new_place1 = k+2;
-              if(new_place1 == 11)
-                new_place1 = 1;
-              score1 += new_place1;
-              if(score1 > 21)
-                score1 = 21;
-               games[i][j][k][l].first += games[score1-1][j][new_place1][l].first * weight;
-               games[i][j][k][l].second += games[score1-1][j][new_place1][l].second * weight;
-               if(score1 == 21)
-                 continue;
-
-              int score2 = j + 1;
-              int new_place2 = l+2;
-              if(new_place2 == 11)
-                new_place2 = 1;
-              score2 += new_place2;
-              if(score2 > 21)
-                score2 = 21;
-               games[score1-1][j][k][l].first += games[score1-1][score2-1][new_place1-1][new_place2-1].first * weight;
-               games[score1-1][j][k][l].second += games[score1-1][score2-1][new_place1-1][new_place2-1].second * weight;
-            }
-          }
-        }
-      }
-    }
-    cout << games[0][0][p1][p2].first << endl;
   } else {
     cout << "Couldn't open file" << endl;
     return false;
@@ -119,8 +116,7 @@ bool day21(long& part1, long& part2)
 }
 
 #ifdef MAIN
-int main()
-{
+int main() {
   long p1, p2;
   day21(p1, p2);
   cout << p1 << endl;
